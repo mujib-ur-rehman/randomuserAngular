d310cc9050276b8635793758a90121056f43ec1b
mujib-ur-rehman <mujib.rehman@focusteck.com>
2021-11-04 16:27:52 +0500

    Using rxjs library function map and filters. Also used BehavioralSubject to get better understanding of it

--- src/app/displayuser/displayuser.component.ts
+++ src/app/displayuser/displayuser.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiserviceService } from '../apiservice.service';
 import { usermodel } from '../usermodel';
-import { AsyncSubject, Subject, BehaviorSubject } from 'rxjs';
+import { AsyncSubject, Subject, BehaviorSubject, ReplaySubject } from 'rxjs';
 
 @Component({
   selector: 'app-displayuser',
@@ -13,8 +13,17 @@ export class DisplayuserComponent implements OnInit {
 
   users: usermodel[] = [];
   user: any = 0;
-  subject = new BehaviorSubject(this.user);
+  subject = new ReplaySubject(3);
   getuser() {
+    this.subject.next(
+      this.service.getuser().subscribe(res => {
+        this.user = res;
+        console.warn(this.user);
+        this.users.push(this.user);
+
+        console.warn(this.users);
+      })
+    );
     this.subject.next(
       this.service.getuser().subscribe(res => {
         this.user = res;
